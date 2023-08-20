87d8f7afc28d2c4f471fae54a03a705c56a1361e
yapna-youpi <yapnayoupi@gmail.com>
2023-08-20 15:16:16 +0100

    ajout des liens sur chaques livre a affiche

--- src/components/addons/cards/Card.jsx
+++ src/components/addons/cards/Card.jsx
@@ -1,24 +1,40 @@
 import React from 'react'
 // import {UisStar } from "@iconscout/react-unicons-solid  "
 import { powerTable } from '../../../Utils'
-import imag1 from '../../home/assets/9782266320726ORI.jpg'
 
-function Card({ data, imag, value }) {
+function Card({ data, imag, value,url }) {
 
   return (
     <div className="carte shadow-lg md:shadow-none md:hover:shadow-lg sm:w-full md:w-6/12 lg:w-3/12  mx-auto border cursor-pointer">
-      <img
-        className="img-produits w-full  h-64  lg:w-70 md:bject-cover md:object-center md:group-hover:opacity-75"
-        src={imag}
-        alt="Olive drab green ."
-      />
+      <a target="_blank" href={url}>
+        <img
+          className="img-produits w-full  h-64  lg:w-70 md:bject-cover md:object-center md:group-hover:opacity-75"
+          src={imag}
+          alt="Olive drab green ."
+        />
+      </a>
       <h3 class="mt-4 text-sm text-gray-700">{data.type}</h3>
-      <div className='flex flex-row'>
-        { powerTable(value).map((item,index)=>{
-            return (<svg xmlns="http://www.w3.org/2000/svg" width={25} enable-background="new 0 0 24 24" viewBox="0 0 24 24" id="star"><path fill="#ff8c00" d="M22,10.1c0.1-0.5-0.3-1.1-0.8-1.1l-5.7-0.8L12.9,3c-0.1-0.2-0.2-0.3-0.4-0.4C12,2.3,11.4,2.5,11.1,3L8.6,8.2L2.9,9C2.6,9,2.4,9.1,2.3,9.3c-0.4,0.4-0.4,1,0,1.4l4.1,4l-1,5.7c0,0.2,0,0.4,0.1,0.6c0.3,0.5,0.9,0.7,1.4,0.4l5.1-2.7l5.1,2.7c0.1,0.1,0.3,0.1,0.5,0.1v0c0.1,0,0.1,0,0.2,0c0.5-0.1,0.9-0.6,0.8-1.2l-1-5.7l4.1-4C21.9,10.5,22,10.3,22,10.1z"></path></svg>)
-        }) }
+      <div className="flex flex-row">
+        {powerTable(value).map((item, index) => {
+          return (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={25}
+              enable-background="new 0 0 24 24"
+              viewBox="0 0 24 24"
+              id="star"
+            >
+              <path
+                fill="#ff8c00"
+                d="M22,10.1c0.1-0.5-0.3-1.1-0.8-1.1l-5.7-0.8L12.9,3c-0.1-0.2-0.2-0.3-0.4-0.4C12,2.3,11.4,2.5,11.1,3L8.6,8.2L2.9,9C2.6,9,2.4,9.1,2.3,9.3c-0.4,0.4-0.4,1,0,1.4l4.1,4l-1,5.7c0,0.2,0,0.4,0.1,0.6c0.3,0.5,0.9,0.7,1.4,0.4l5.1-2.7l5.1,2.7c0.1,0.1,0.3,0.1,0.5,0.1v0c0.1,0,0.1,0,0.2,0c0.5-0.1,0.9-0.6,0.8-1.2l-1-5.7l4.1-4C21.9,10.5,22,10.3,22,10.1z"
+              ></path>
+            </svg>
+          );
+        })}
       </div>
-      <p class="mt-1 text-2xl font-serif font-medium text-gray-900">{data.author}</p>
+      <p class="mt-1 text-2xl font-serif font-medium text-gray-900">
+        {data.author}
+      </p>
       <p class="mt-1 text-lg font-medium text-gray-900">{data.title}</p>
     </div>
   );
--- src/components/home/parution/Parution.jsx
+++ src/components/home/parution/Parution.jsx
@@ -1,28 +1,48 @@
 import React from 'react'
 import Card from '../../addons/cards/Card'
-import tof1 from "../assets/magap.png"
-import tof2 from "../assets/9782266320726ORI.jpg"
-import tof3 from "../assets/9782365697323ORI.jpg"
-import tof4 from "../assets/9782749174686ORI.jpg"
+import tof1 from "../../../Data/livres/lula.png"
+import tof2 from "../../../Data/livres/main.png"
+import tof3 from "../../../Data/livres/lula.png"
+import tof4 from "../../../Data/livres/lula.png"
 
 function Auteurs() {
 
-    const data = {
+    const LivreParu = [
+      {
         type: "polar serein",
-        author: "MAGAPCHE",
-        title: "Père et Fille"
-    }
-
+        author: "Boudjeka Kamto",
+        title: "Lula aime les livres",
+      },
+      {
+        type: "polar serein",
+        author: "Boudjeka Kamto",
+        title: "La Main tendue",
+      },
+      {
+        type: "polar serein",
+        author: "Boudjeka Kamto",
+        title: "Lula aime les livres",
+      },
+      {
+        type: "polar serein",
+        author: "Boudjeka Kamto",
+        title: "Lula aime les livres",
+      },
+    ];
 
   return (
     <>
-      <h2 className="font-semibold px-4 mt-4  text-4xl md:mt-16 md:pl-14 md:ml-1 ">Dernières Parutions</h2>
-      <p className='py-5 px-4 md:px-0 md:pl-14 md:ml-1 font-semibold'>Pour ne rien manquer des dernières sorties et celles à venir !</p>
+      <h2 className="font-semibold px-4 mt-4  text-4xl md:mt-16 md:pl-14 md:ml-1 ">
+        Dernières Parutions
+      </h2>
+      <p className="py-5 px-4 md:px-0 md:pl-14 md:ml-1 font-semibold">
+        Pour ne rien manquer des dernières sorties et celles à venir !
+      </p>
       <div className="flex flex-wrap md:flex-row w-11/12 mx-auto justify-around">
-        <Card data={data} imag={tof1} value={5} />
-        <Card data={data} imag={tof2} value={4} />
-        <Card data={data} imag={tof3} value={4}/>
-        <Card data={data} imag={tof4} value={5} />
+        <Card data={LivreParu[0]} imag={tof1} value={5} url="https://www.amazon.fr/LULA-aime-livres-Boudjeka-KAMTO-ebook/dp/B0CFB452MP/ref=mp_s_a_1_1?crid=3GBS56R5YQAT3&keywords=lula+aime+les+livres&qid=1692008040&sprefix=%2Caps%2C215&sr=8-1" />
+        <Card url="https://amzn.eu/d/5iLlP6Z" data={LivreParu[1]} imag={tof2} value={4} />
+        <Card data={LivreParu[2]} imag={tof3} value={4} />
+        <Card data={LivreParu[3]} imag={tof4} value={5} />
         {/* <Card data={data} imag="https://picsum.photos/400/484" /> */}
       </div>
     </>
